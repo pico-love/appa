c8dcc020ca1ab925d60a1b8f005d322b3f5953a1
ulysses316 <ulises316@live.com.mx>
2025-09-02 21:00:50 -0600

    [ADD] Carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,11 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 0,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3
+      }
+    },
     navigation: {
       nextEl: ".carousel-button-next",
       prevEl: ".carousel-button-prev",
